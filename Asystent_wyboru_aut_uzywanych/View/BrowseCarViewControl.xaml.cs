33a383fcdb7712b5bf695fd37e9efba102d868aa
Marcin Ciszek <marccis558@student.polsl.pl>
2021-07-07 18:27:59 +0200

    Utworzenie widoków dla kontrolek (dodawania, usuwania i przegladania aut)

---
+++ Asystent_wyboru_aut_uzywanych/View/BrowseCarViewControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asystent_wyboru_aut_uzywanych.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy BrowseCarViewControl.xaml
+    /// </summary>
+    public partial class BrowseCarViewControl : UserControl
+    {
+        public BrowseCarViewControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
